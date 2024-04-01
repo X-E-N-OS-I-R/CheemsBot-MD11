96cb85e35d030fa13f1a567cd724790d3221ba1f
xenoser <147225788+X-E-N-OS-I-R@users.noreply.github.com>
2024-04-01 21:08:20 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,13 +19,13 @@ global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
-global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
+global.botname = 'XENO SIR' //ur bot name
+global.ownernumber = '918136810956' //ur owner number
+global.ownername = '🦄XENO SIR' //ur owner name
 global.websitex = "https://youtu.be/mu5m6aB6P5k"
 global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
-global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
+global.themeemoji = '🗿'
+global.wm = "XENO SIR."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD11' //script link
 global.packname = "Sticker By"
 global.author = "🦄드림 가이 Xeon\n\n+916909137213"
